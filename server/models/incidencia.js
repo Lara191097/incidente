82a6e75a517cb0c5415cd8940ce4f78be55eb040
Juan Pablo Marengo <juanpmarengo@gmail.com>
2016-10-22 15:17:48 -0300

    Change in model 2

--- server/models/incidencia.js
+++ server/models/incidencia.js
@@ -3,13 +3,13 @@ var mongoose = require('mongoose');
 module.exports = mongoose.model('Incidencia', {
     laboratorio: String,
     maquina: String,
-    sistemaOperativo: {
-        nombre: String,
-        virtual: Boolean,
-        comentario: String
-    },
     fallas: [{
         fecha: { type: Date, default: Date.now },
+        sistemaOperativo: {
+            nombre: String,
+            virtual: Boolean,
+            comentario: String
+        },
         codigo: String,
         descripcion: String,
         estado: String,
